1c224c804a679fbec67aae665ed85446870aac62
Timm Bangma <tbangma@ea.com>
2024-05-08 11:52:08 -0400

    adding FZF

--- init.lua
+++ init.lua
@@ -10,7 +10,9 @@ require('lazy').setup({
     {'hrsh7th/nvim-cmp'},
     {'L3MON4D3/LuaSnip'},
     { "williamboman/mason.nvim" },
-    { 'williamboman/mason-lspconfig.nvim' }
+    { 'williamboman/mason-lspconfig.nvim' },
+    { 'junegunn/fzf' },
+    { 'junegunn/fzf.vim' }
 })
 require('lsp-config')
 require('treesitter-config')
--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -1,13 +1,13 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
+  _ = client
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
 local lua_opts = lsp_zero.nvim_lua_ls()
 require('lspconfig').lua_ls.setup(lua_opts)
 require('lspconfig').zls.setup({})
-require('lspconfig').kotlin_language_server.setup({})
 require('lspconfig').ols.setup({})
 
 require('mason').setup({})
