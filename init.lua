d575ab548cf31dbb3f0ac19318ec77b38ee2596d
Timm Bangma <tbangma@ea.com>
2024-08-22 10:15:41 -0400

    tidy

--- init.lua
+++ init.lua
@@ -1,3 +1,29 @@
+--- downloads plugins from github, and adds them to the runtime path.
+local function packadd(repo_list)
+	for _, repo_name in pairs(repo_list) do
+		local repo = 'https://github.com/' .. repo_name
+		local outdir = vim.fn.stdpath('data') .. "/" .. repo_name
+
+		if not vim.uv.fs_stat(outdir) then
+			print('Downloading [' .. repo_name .. '] to [' .. outdir .. ']')
+			vim.fn.system({ 'git', 'clone', repo, outdir })
+		end
+		vim.opt.rtp:prepend(outdir)
+	end
+end
+
+--- activates lsp client configs
+local function lsp(lsp_list)
+	local cfg = require('lspconfig')
+	for _, lsp_name in pairs(lsp_list) do cfg[lsp_name].setup {} end
+end
+
+--- shortcut for nnoremap silent
+local function nmap(keys, fn)
+	local opts = { noremap = true, silent = true }
+	vim.api.nvim_set_keymap('n', keys, ':lua ' .. fn .. '<CR>', opts)
+end
+
 local leader_key = ';'
 vim.g.mapleader = leader_key
 vim.g.maplocalleader = leader_key
@@ -12,28 +38,6 @@ vim.opt.signcolumn = "yes:1"
 
 vim.opt.mouse = ""
 
--- conjure scheme client configuration
-vim.g["conjure#mapping#doc_word"] = false
-local scheme_client = 'conjure#client#scheme#stdio#'
-vim.g[scheme_client .. 'command'] = "chibi-scheme"
-vim.g[scheme_client .. 'value_prefix_pattern'] = ""
-vim.g[scheme_client .. 'prompt_pattern'] = "> "
-
--- assumes github. makes things simpler
-local function packadd(repo_list)
-	for _, repo_name in pairs(repo_list) do
-		local repo = 'https://github.com/' .. repo_name
-		local outdir = vim.fn.stdpath('data') .. "/" .. repo_name
-
-		if not vim.uv.fs_stat(outdir) then
-			print('Downloading [' .. repo_name .. '] to [' .. outdir .. ']')
-			vim.fn.system({ 'git', 'clone', repo, outdir })
-		end
-		vim.opt.rtp:prepend(outdir)
-	end
-end
-
--- plugins
 packadd({
 	'junegunn/fzf',
 	'junegunn/fzf.vim',
@@ -45,17 +49,19 @@ packadd({
 
 vim.cmd.colorscheme("quiet-extended")
 
--- lsp configuration
-require('lspconfig').lua_ls.setup {}
-require('lspconfig').zls.setup {}
+local scheme_client = 'conjure#client#scheme#stdio#'
+vim.g["conjure#mapping#doc_word"] = false
+vim.g[scheme_client .. 'command'] = "chibi-scheme"
+vim.g[scheme_client .. 'value_prefix_pattern'] = false
+vim.g[scheme_client .. 'prompt_pattern'] = "> "
+
+lsp({ 'lua_ls', 'zls', 'gopls' })
 
 vim.diagnostic.config { virtual_text = false, underline = false }
-vim.api.nvim_set_keymap('n', '<leader><leader>', ':lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true })
 
--- treesitter config
+nmap('<leader><leader>', 'vim.lsp.buf.format()')
+nmap('gD', 'vim.lsp.buf.definition()')
+
 require('nvim-treesitter.configs').setup {
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
+	highlight = { enable = true, additional_vim_regex_highlighting = false },
 }
